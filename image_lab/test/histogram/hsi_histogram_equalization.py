a193ab38a3de390b8443223155e34a26664b79f6
jim2107054 <mdjahid205hasan@gmail.com>
2026-09-02 13:22:33 +0600

    feat: implement HSI color space intensity histogram equalization script

--- image_lab/test/histogram/hsi_histogram_equalization.py
+++ image_lab/test/histogram/hsi_histogram_equalization.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('input.jpeg')
+img = cv2.imread(r"image_lab/test/input.jpeg")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
